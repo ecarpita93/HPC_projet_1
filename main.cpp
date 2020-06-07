b2fe3d734df37fa92ddbb0eca480e7ac569e4c36
ecarpita93 <ecarpita93@outlook.com>
2020-06-07 23:29:52 +0200

    HPC projet 1a

---
+++ main.cpp
@@ -0,0 +1,39 @@
+/*
+
+Edoardo Carpita
+HPC projet 1 - travail d'amelioration sur la librarie PICCANTE
+
+*/
+
+#include "include/piccante.hpp"
+
+int main(int argc, char *argv[])
+{
+
+    printf("Adding file names to the merger...");
+    pic::HDRMerger merger;
+
+    for(int i = 0; i < 7; i++) {
+        std::string name = "../data/input/stack/stack_room_exp_" + pic::fromNumberToString(i) + ".jpg";
+        merger.addFile(name);
+    }
+
+    printf("\nOk one\n");
+
+    printf("Merging LDR images into an HDR image...");
+    pic::Image *imgOut = merger.execute(NULL);
+    printf("\nOk two\n");
+
+    if(imgOut != NULL) {
+        if(imgOut->isValid()) {
+            imgOut->Write("../data/output/image_debevec_crf.hdr");
+            pic::Image *imgTmo = pic::ReinhardTMO::executeGlobal1(imgOut, NULL);
+            imgTmo->Write("../data/output/image_debevec_crf_tmo.png", pic::LT_NOR_GAMMA);
+            delete imgTmo;
+            delete imgOut;
+        }
+    }
+    printf("\nOk done\n");
+
+    return 0;
+}
